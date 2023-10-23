1e9a97956858b64445e7b02c79fb1c1c6be570bb
joelcena <joelcena.47@gmail.com>
2023-10-23 23:37:14 +0300

    ft-responsive

--- components/PageMetaData.tsx
+++ components/PageMetaData.tsx
@@ -7,7 +7,7 @@ const PageMetaData = (): JSX.Element => {
 
       <title>Car Rental</title>
       <meta name='Description' content='Car Rental WebSite' />
-      {/* <link rel="shortcut icon" href="https://ik.imagekit.io/joel/hilux.jpg?updatedAt=1697712037718" type="image/x-icon" /> */ }
+      <link rel="shortcut icon" href="https://ik.imagekit.io/joel/hilux.jpg?updatedAt=1697712037718" type="image/x-icon" />
     </Head>
   );
 };
