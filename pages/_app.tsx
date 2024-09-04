a66cc6ef46e848528eb17fe176072c5b4fb6ba45
joelkimr <joelcena.47@gmail.com>
2024-09-04 12:35:45 +0300

    ft-cars

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,22 +4,26 @@ import Scaffold from '../components/Scaffold';
 import PageMetaData from '../components/PageMetaData';
 import '../styles/globals.css';
 
-// import { Montserrat } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
+import NavBar from '../components/HeroSection/navBar';
 
-// const montserrat = Montserrat({
-//   subsets: ['latin'],
-//   variable: '--font-mont',
-// });
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-mont',
+});
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      {/* <main className={ `${montserrat.variable} font-mont w-full h-full lg:min-w-full md:min-w-[150%] min-w-[300%] mx-auto overflow-x-hidden` }> */}
-      <PageMetaData />
-      <Scaffold>
-        <Component {...pageProps} />
-      </Scaffold>
-      {/* </main> */}
+      <main
+        className={`${montserrat.variable} font-mont w-full h-full lg:min-w-full md:min-w-[150%] min-w-[300%] mx-auto overflow-x-hidden`}
+      >
+        <PageMetaData />
+        <Scaffold>
+          <NavBar />
+          <Component {...pageProps} />
+        </Scaffold>
+      </main>
     </>
   );
 };
