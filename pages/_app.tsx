ac6625a71e533fd3c02e26b1b1a724464160da47
joelkimr <joelcena.47@gmail.com>
2024-09-12 17:24:06 +0300

    ft-clerk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,13 @@ import '../styles/globals.css';
 
 import { Montserrat } from 'next/font/google';
 import NavBar from '../components/HeroSection/navBar';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -15,15 +22,25 @@ const montserrat = Montserrat({
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <main
-        className={`${montserrat.variable} font-mont w-full h-full lg:min-w-full md:min-w-[150%] min-w-[300%] mx-auto overflow-x-hidden`}
-      >
-        <PageMetaData />
-        <Scaffold>
-          <NavBar />
-          <Component {...pageProps} />
-        </Scaffold>
-      </main>
+      <ClerkProvider>
+        <main
+          className={`${montserrat.variable} font-mont w-full h-full lg:min-w-full md:min-w-[150%] min-w-[300%] mx-auto overflow-x-hidden`}
+        >
+          <PageMetaData />
+          <div className='absolute '>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+          <Scaffold>
+            <NavBar />
+            <Component {...pageProps} />
+          </Scaffold>
+        </main>
+      </ClerkProvider>
     </>
   );
 };
